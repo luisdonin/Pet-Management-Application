02b80bc5307a9eaed3570bf1b67cb28e1c553857
0x000nullpointer <donin@alunos.utfpr.edu.br>
2025-06-02 13:54:44 +0000

    Built Human API

--- app/src/main/java/com/example/pets/api/Humans/HuAPI.java
+++ app/src/main/java/com/example/pets/api/Humans/HuAPI.java
@@ -1,4 +1,17 @@
 package com.example.pets.api.Humans;
 
+import java.util.List;
+
+import com.example.pets.model.Human;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface HuAPI {
+    @GET("human")
+    Call<List<Human>> getHuman();
+
+    @GET("Human/{id}")
+    Call<Human> getHuman(@Path("id") int id);
 }
